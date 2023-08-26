fef8f24f08e5d63d47f9e7d2b9f61b7a8d2dbf82
cceer <sisir.sital@gmail.com>
2023-08-26 17:12:15 +0545

    feat: added close logic for socket connection

--- Backend_communication_patterns/Push.js
+++ Backend_communication_patterns/Push.js
@@ -15,9 +15,18 @@ const websocket = new WebSocketServer({ httpServer: server });
 websocket.on('request', (request) => {
     const connection = request.accept(null, request.origin);
 
+    connection.on('close', () => {
+        const portAddress = connection.socket.remotePort;
+
+        connections = connections.filter((c) => c.socket.remotePort !== portAddress);
+
+        connections.forEach((c) => c.send(`User ${connection.socket.remotePort} has left`));
+    });
+
     connection.on('message', (message) => {
         connections.forEach((c) => c.send(`User ${connection.socket.remotePort} says: ${message.utf8Data}`));
     });
+
     connections.push(connection);
 
     connections.forEach((c) => c.send(`User ${connection.socket.remotePort} has joined`));
